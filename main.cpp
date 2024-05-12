554ffb0cec5922bef3fd361e55df3c8262fce862
samuel <samuel.svancarek>
2024-05-12 20:56:06 +0200

    implemented bubbleSort

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@ double returnTimeInMs() {
     return 1.2;
 }
 
+int bubbleSort() {
+    cout << "sorted" << endl;
+};
+
 int main() {
     cout << "Hello, World!" << endl;
     cout << returnTimeInMs() << endl;
-
+    bubbleSort();
     return 0;
 }
