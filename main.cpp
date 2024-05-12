9a1ea2245e499bc921ae15bdd43039478cc42c0d
samuel <samuel.svancarek>
2024-05-12 21:03:59 +0200

    implemented returnAuthors fun

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@ double returnTimeInMs() {
     return 1.2;
 }
 
+string returnAuthors() {
+    return "Samuel Svancarek";
+}
+
 int main() {
     cout << "Hello, World!" << endl;
     cout << returnTimeInMs() << endl;
-
+    cout << returnAuthors() << endl;
     return 0;
 }
